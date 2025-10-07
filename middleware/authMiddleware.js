bb0fc1a881fe54fb67a20995ab847de63cfbe048
pakinphanpaw <pakinphanpaw@gmail.com>
2025-10-07 20:27:14 +0700

    fix increased size image

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,11 +4,11 @@ function authMiddleware(req, res, next) {
   const authHeader = req.headers["authorization"];
   if (!authHeader) return res.status(401).json({ message: "No token provided" });
 
-  const token = authHeader.split(" ")[1];
+  const token = authHeader.split(" ")[1]; //ตัด bearer ออกเหลือแค่ token
   if (!token) return res.status(401).json({ message: "Invalid token format" });
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); // ใช้ฟังก์ชัน jwt.verify() เพื่อ “ถอดรหัสและตรวจสอบความถูกต้องของ token”
     req.user = decoded;
     next();
   } catch (err) {
--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
