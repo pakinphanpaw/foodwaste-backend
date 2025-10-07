8f5c749e1f1cba3de836d35e0261cd75e7098ee8
pakinphanpaw <pakinphanpaw@gmail.com>
2025-10-07 20:51:49 +0700

    fix image

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 
-app.use(express.json());
+
 app.use(cors());
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ limit: '20mb', extended: true }));
